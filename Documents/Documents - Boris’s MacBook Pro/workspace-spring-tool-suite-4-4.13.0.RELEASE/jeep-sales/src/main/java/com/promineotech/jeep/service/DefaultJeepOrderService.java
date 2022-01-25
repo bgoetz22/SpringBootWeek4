55796939d09af77e88327dfa9622e3c53a97602d
bgoetz22 <boris.goetz@gmail.com>
2022-01-24 21:32:41 -0700

    code end of week 4 STS

---
+++ Documents/Documents - Boris’s MacBook Pro/workspace-spring-tool-suite-4-4.13.0.RELEASE/jeep-sales/src/main/java/com/promineotech/jeep/service/DefaultJeepOrderService.java
@@ -0,0 +1,146 @@
+package com.promineotech.jeep.service;
+
+import java.math.BigDecimal;
+import org.apache.el.lang.ELArithmetic.BigDecimalDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.promineotech.jeep.dao.JeepOrderDao;
+import com.promineotech.jeep.entity.Color;
+import com.promineotech.jeep.entity.Customer;
+import com.promineotech.jeep.entity.Engine;
+import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.Option;
+import com.promineotech.jeep.entity.Order;
+import com.promineotech.jeep.entity.OrderRequest;
+import com.promineotech.jeep.entity.Tire;
+
+@Service
+public class DefaultJeepOrderService implements JeepOrderService {
+
+  @Autowired
+  private JeepOrderDao jeepOrderDao;
+  
+  @Transactional
+  @Override
+  public Order createOrder(OrderRequest orderRequest) {
+    Customer customer = getCustomer(orderRequest);
+
+    Jeep jeep = getModel(orderRequest);;
+
+    Color color = getColor(orderRequest);
+
+    Engine engine = getEngine(orderRequest);
+
+    Tire tire = getTire(orderRequest);
+    List<Option> options = getOption(orderRequest);
+
+    BigDecimal price = jeep.getBasePrice().add(color.getPrice())
+        .add(engine.getPrice()).add(tire.getPrice());
+    
+    for(Option option : options) {
+      price = price.add(option.getPrice());
+    }
+    
+    return jeepOrderDao.saveOrder(customer, jeep, color, engine, tire, price, 
+        options); 
+  }
+  
+  /**
+   * 
+   * @param orderRequest
+   * @return
+   */
+  private List<Option> getOption(OrderRequest orderRequest) {
+    return jeepOrderDao.fetchOptions(orderRequest.getOptions());
+  }
+
+  /**
+   * 
+   * @param orderRequest
+   * @throws X
+   */
+
+  protected void getTire(OrderRequest orderRequest) throws X {
+    jeepOrderDao.fetchTire(orderRequest.getTire())
+        .orElseThrow(() -> new NoSuchElementException(
+            "Tire with ID=" + orderRequest.getTire() + " was not found"));
+  }
+  
+  /**
+   * 
+   * @param orderRequest
+   * @throws X
+   */
+
+  protected void getEngine(OrderRequest orderRequest) throws X {
+    jeepOrderDao.fetchEngine(orderRequest.getEngine())
+        .orElseThrow(() -> new NoSuchElementException(
+            "Engine with ID=" + orderRequest.getEngine() + " was not found"));
+  }
+
+  /**
+   * 
+   * @param orderRequest
+   * @throws X
+   */
+  protected void getColor(OrderRequest orderRequest) throws X {
+    jeepOrderDao.fetchColor(orderRequest.getColor())
+        .orElseThrow(() -> new NoSuchElementException(
+            "Color with ID=" + orderRequest.getColor() + " was not found"));
+  }
+  
+  /**
+   * 
+   * @param orderRequest
+   * @throws X
+   */
+
+  protected void getModel(OrderRequest orderRequest) throws X {
+    jeepOrderDao
+        .fetchModel(orderRequest.getModel(), orderRequest.getTrim(),
+            orderRequest.getDoors())
+        .orElseThrow(() -> new NoSuchElementException("Model with ID="
+            +   orderRequest.getModel() + ", trim=" + orderRequest.getTrim()
+            +   orderRequest.getDoors() + " was not found"));
+  }
+  
+  /**
+   * 
+   * @param orderRequest
+   * @throws X
+   */
+
+  protected void getCustomer(OrderRequest orderRequest) throws X {
+    jeepOrderDao.fetchCustomer(orderRequest.getCustomer())
+        .orElseThrow(() -> new NoSuchElementException("Customer with ID="
+            +   orderRequest.getCustomer() + " was not found"));
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
